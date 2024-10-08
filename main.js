291d6432e6fb3d1ed544ad926bb3bd1677e021f6
IvanVitali89 <ertre89@gmail.com>
2024-10-08 09:30:22 +0200

    add prompt

---
+++ main.js
@@ -0,0 +1,7 @@
+//chiedere i dati iniziano
+const age = prompt("Inserisci la tua età") 
+//verificare che i dati siano numeri !(isNan)
+//salvare i dati in due variabili age kilometres
+//se l'età è minore di 18 anni allora (kilometres * 0.21)-20%
+//se l'età è maggiore di 65 allora (kilometres * 0.21)-40%
+//.toFixed(2);
